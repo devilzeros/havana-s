83be4b0036d1c0a229a7f1ce2e10ab9ca9cd32eb
nattavee <devilzeros@outlook.co.th>
2019-08-05 21:36:29 +0700

    make it by chun

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var cors = require('cors');
 app.use(bodyParser.json());
 app.use(cors());
 
+var port = process.env.PORT || 3001;
+
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -29,8 +31,8 @@ app.use((req, res, next) => {
   }
 });
 
-app.listen(3001, () => {
-  console.log('API Start server at port 3001.')
+app.listen(port, () => {
+  console.log('API Start server at port: ', port)
 })
 
 var publicDir = require('path').join(__dirname,'./image');
